b1f77c5effa0c86a554a9eabc1ec9902d0b88738
rseeli <64947421+romanseeli@users.noreply.github.com>
2025-10-27 17:22:32 +0100

    change config

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define ('PAGENAME_INPUT', 'index.php');
 define ('PAGENAME_RESULT', 'result.php');
 
-define ('TEST_DEBUG', true);    // sets the following values for easier testing in Sandbox
+define ('TEST_DEBUG', false);    // sets the following values for easier testing in Sandbox
 define ('TEST_MERCHANTID', '1100017764');
 define ('TEST_APIPWD', 'AIyTP45GDYEH9y2x');
 define ('TEST_AMOUNT', '650');
@@ -11,6 +11,6 @@ define ('TEST_CURRENCY', 'CHF');
 define ('TEST_REFNO', 'test-OB-1234');
 
 // Datatrans configuration
-define('DT_HOST', 'https://api.sandbox.datatrans.com');
+define('DT_HOST', 'https://api.datatrans.com');
 
 ?>
